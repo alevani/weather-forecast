172e16ae536c788ffe78ac19da75c1bd8e879bd8
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 15:54:34 +0200

    Add URI debug

--- src/services/open_weather_api.rs
+++ src/services/open_weather_api.rs
@@ -18,6 +18,15 @@ pub fn fetch_current_weather(https_client: &HttpsClient, city: &City) -> Respons
         open_weather_api_key
     );
     
+    // for debug purpose
+    println!("{}", format!(
+        "{}/onecall?lat={}&lon={}&appid={}&units=metric",
+        open_weather_api_url,
+        city.lat,
+        city.long,
+        open_weather_api_key
+    ));
+
     let req = Request::builder()
     .method(Method::GET)
     .uri(uri)
