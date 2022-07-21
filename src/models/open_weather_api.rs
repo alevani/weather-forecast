aac0f90666a4bd516d11697e8a00821cb65950d5
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 15:54:55 +0200

    Add weather conditions (and optionally alerts..)

--- src/models/open_weather_api.rs
+++ src/models/open_weather_api.rs
@@ -2,7 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct OpenWeatherApiOneCallResponse {
-    pub current: CurrentWeatherData
+    pub current: CurrentWeatherData,
+    pub alerts: Option<Vec<AlertsData>>
 }
 
 #[derive(Serialize, Deserialize)]
@@ -10,5 +11,21 @@ pub struct CurrentWeatherData {
     pub sunrise: i64,
     pub sunset: i64,
     pub temp: f32,
-    pub feels_like: f32
+    pub feels_like: f32,
+    pub clouds: i32,
+    pub weather: Vec<WeatherData>,
+    pub uvi: f32
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AlertsData {
+    pub sender_name: String,
+    pub event: String,
+    pub description: String
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct WeatherData {
+    pub main: String,
+    pub description: String,
 }
