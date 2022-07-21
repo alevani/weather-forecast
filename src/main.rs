0b414d8b33d7d016fcc21c508ee216831f072325
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 09:16:57 +0200

    Add Base Hyper request dummy code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,40 @@
-use std::env;
+use std::{env};
+extern crate pretty_env_logger;
+
+use hyper::{Client, Request, Method, Body, Response, Error, client::{ResponseFuture, HttpConnector}};
+use hyper_tls::HttpsConnector;
+
+// use std::str::FromStr;
 
 pub mod utils;
 use utils::Env;
 
-fn main() {
+type HttpsClient = Client<HttpsConnector<HttpConnector>>;
 
-    let api_key = env::var(Env::OpenWeatherApiKey.value());
-    println!("Hello, world!");
+async fn fetch_current_weather(https_client: &HttpsClient) -> ResponseFuture {
+    
+    let uri = format!();
 
+    let req = Request::builder()
+    .method(Method::GET)
+    // .uri(format!(
+    //     "{}/MemberIdentities/{}?code={}&idtype={}",
+    //     get_env(Env::MisBaseUrl),
+    //     user_uuid,
+    //     get_env(Env::MisFunctionKey),
+    //     user_login_method_type
+    // ))
+    .body(Body::empty()).unwrap(); // todo match here
+
+    https_client.request(req)
+}
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    // let api_key = env::var(Env::OpenWeatherApiKey.value());
+    let https = HttpsConnector::new();
+    let https_client = Client::builder().build(https);
+    fetch_current_weather(&https_client).await;
 }
