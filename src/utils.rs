98b3b75522f4dd364e7fe2ecece0ea2735f21903
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 10:06:37 +0200

    Add Hyper response parser

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,6 @@
+use hyper::{Body, Response};
+use hyper::body as BodyParser;
+
 pub enum Env {
     OpenWeatherApiKey,
     OpenWeatherApiUrl
@@ -9,5 +12,15 @@ impl Env {
             Env::OpenWeatherApiKey => "OPEN_WEATHER_API_KEY",
             Env::OpenWeatherApiUrl => "OPEN_WEATHER_API_URL",
         }
+    }
+}
+
+async fn parse_response<T: serde::de::DeserializeOwned>(response: Response<Body>) -> Result<T, String> {
+    match BodyParser::to_bytes(response.into_body()).await {
+        Ok(content) => match serde_json::from_slice::<T>(&content) {
+            Ok(parsed_content) => Ok(parsed_content),
+            Err(err) => Err(err.to_string()),
+        },
+        Err(err) => Err(err.to_string()),
     }
 }
