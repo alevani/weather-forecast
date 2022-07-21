9f6152b0de4fc2fc21ad0cfc150b141589602877
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 11:16:21 +0200

    Add logic to get current weather from openWeatherAPI

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::{env};
 extern crate pretty_env_logger;
 
 use hyper::{Client, Request, Method, Body, Response, client::{ResponseFuture, HttpConnector}};
 use hyper_tls::HttpsConnector;
 
-// use std::str::FromStr;
+use chrono::{Datelike, Timelike, Utc};
 
 pub mod utils;
-use utils::Env;
+use utils::{Env, parse_response, get_env};
 
 type HttpsClient = Client<HttpsConnector<HttpConnector>>;
 
@@ -17,15 +16,34 @@ pub struct City {
     name: String
 }
 
-async fn fetch_current_weather(https_client: &HttpsClient, city: &City) -> ResponseFuture {
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct OpenWeatherApiOneCallResponse {
+    current: CurrentWeatherData
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CurrentWeatherData {
+    sunrise: i32,
+    sunset: i32,
+    temp: f32,
+    feels_like: f32
+}
+
+fn fetch_current_weather(https_client: &HttpsClient, city: &City) -> ResponseFuture {
+    let open_weather_api_key =  get_env(Env::OpenWeatherApiKey);
+    let open_weather_api_url =  get_env(Env::OpenWeatherApiUrl);
+    
     
     let uri = format!(
-        "{}/onecall=lat{}&lon={}&appid={:?}",
-        Env::OpenWeatherApiUrl.value(),
-        city.lat, city.long,
-        env::var(Env::OpenWeatherApiKey.value())
+        "{}/onecall?lat={}&lon={}&appid={}",
+        open_weather_api_url,
+        city.lat,
+        city.long,
+        open_weather_api_key
     );
-
+    
     let req = Request::builder()
     .method(Method::GET)
     .uri(uri)
@@ -40,9 +58,30 @@ async fn fetch_current_weather(https_client: &HttpsClient, city: &City) -> Respo
 async fn main() {
     pretty_env_logger::init();
 
-    let copenhagen = City { lat: 55.676098, long: 12.568337, name: String::from("Copenhagen") };
+    let city = City { lat: 55.676098, long: 12.568337, name: String::from("city") };
     
     let https = HttpsConnector::new();
     let https_client = Client::builder().build(https);
-    fetch_current_weather(&https_client, &copenhagen).await;
+    let current_weather_raw_data = match fetch_current_weather(&https_client, &city).await {
+        Ok(response_body) => response_body,
+        Err(_) => panic!("Could not fetch OpenWeatherApi current weather for: {}", city.name)
+    };
+
+    let current_weather_data = match parse_response::<OpenWeatherApiOneCallResponse>(current_weather_raw_data).await {
+        Ok(content) => content,
+        Err(_) => panic!("Could not parse content of current_weather_raw_data")
+    };
+
+    print!(
+    "
+    Current weather information for {}:
+    The sun will rise at {} and set at {}
+    It is currently {} but it feels like {}.
+    ",
+    city.name,
+    current_weather_data.current.sunrise,
+    current_weather_data.current.sunset,
+    current_weather_data.current.temp,
+    current_weather_data.current.feels_like
+    );
 }
