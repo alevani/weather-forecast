dcb6df3efd6588ae7f6125da3b49ea2a908a34a6
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 10:06:54 +0200

    Add City struct and call to open weather api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{env};
 extern crate pretty_env_logger;
 
-use hyper::{Client, Request, Method, Body, Response, Error, client::{ResponseFuture, HttpConnector}};
+use hyper::{Client, Request, Method, Body, Response, client::{ResponseFuture, HttpConnector}};
 use hyper_tls::HttpsConnector;
 
 // use std::str::FromStr;
@@ -11,30 +11,38 @@ use utils::Env;
 
 type HttpsClient = Client<HttpsConnector<HttpConnector>>;
 
-async fn fetch_current_weather(https_client: &HttpsClient) -> ResponseFuture {
+pub struct City {
+    lat: f32,
+    long: f32,
+    name: String
+}
+
+async fn fetch_current_weather(https_client: &HttpsClient, city: &City) -> ResponseFuture {
     
-    let uri = format!();
+    let uri = format!(
+        "{}/onecall=lat{}&lon={}&appid={:?}",
+        Env::OpenWeatherApiUrl.value(),
+        city.lat, city.long,
+        env::var(Env::OpenWeatherApiKey.value())
+    );
 
     let req = Request::builder()
     .method(Method::GET)
-    // .uri(format!(
-    //     "{}/MemberIdentities/{}?code={}&idtype={}",
-    //     get_env(Env::MisBaseUrl),
-    //     user_uuid,
-    //     get_env(Env::MisFunctionKey),
-    //     user_login_method_type
-    // ))
+    .uri(uri)
     .body(Body::empty()).unwrap(); // todo match here
 
     https_client.request(req)
 }
 
+
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
-    // let api_key = env::var(Env::OpenWeatherApiKey.value());
+    let copenhagen = City { lat: 55.676098, long: 12.568337, name: String::from("Copenhagen") };
+    
     let https = HttpsConnector::new();
     let https_client = Client::builder().build(https);
-    fetch_current_weather(&https_client).await;
+    fetch_current_weather(&https_client, &copenhagen).await;
 }
