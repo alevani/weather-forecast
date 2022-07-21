a3b3e3a989d91399ea10fca1865bd71b554178ad
Alexandre Vanini <vanini.alexandre@gmail.com>
2022-07-21 11:16:12 +0200

    Rewrite Env parser + Make parse_response public

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,25 @@
 use hyper::{Body, Response};
 use hyper::body as BodyParser;
+use std::env;
 
 pub enum Env {
     OpenWeatherApiKey,
     OpenWeatherApiUrl
 }
 
-impl Env {
-    pub fn value(&self) -> &str {
-        match *self {
-            Env::OpenWeatherApiKey => "OPEN_WEATHER_API_KEY",
-            Env::OpenWeatherApiUrl => "OPEN_WEATHER_API_URL",
-        }
-    }
+pub fn get_env(key: Env) -> String {
+    let key = match key {
+        Env::OpenWeatherApiKey => "OPEN_WEATHER_API_KEY",
+        Env::OpenWeatherApiUrl => "OPEN_WEATHER_API_URL",
+    };
+
+    return match env::var(key) {
+        Ok(base_url) => base_url,
+        Err(_) => panic!("Could not find env key: {}", key)
+    };
 }
 
-async fn parse_response<T: serde::de::DeserializeOwned>(response: Response<Body>) -> Result<T, String> {
+pub async fn parse_response<T: serde::de::DeserializeOwned>(response: Response<Body>) -> Result<T, String> {
     match BodyParser::to_bytes(response.into_body()).await {
         Ok(content) => match serde_json::from_slice::<T>(&content) {
             Ok(parsed_content) => Ok(parsed_content),
